3174f02116f0649291b4c408880feacf462a8c2a
L. David Varvel <dave@cofunder.net>
2013-08-13 15:07:32 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tinyblog::Application.config.secret_key_base = '88d27319a6f17b3f6632fb48ba75633900fbbbdc00c07e8d3d310273b74af58e51430d6a748c8cfdd62c6622dc111c97113d97e39a0d9a8d9686dde11435f4be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tinyblog::Application.config.session_store :cookie_store, key: '_tinyblog_session'
